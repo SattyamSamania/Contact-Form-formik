c259244cfbc2dcdf5cf0a229122dcfd560919bc1
SattyamSamania <Sattyamsamania2810@gmail.com>
2024-01-19 15:26:58 +0530

    Updated Home Page Content

--- app/page.js
+++ app/page.js
@@ -4,11 +4,11 @@ export default function page() {
 
     return (
         <div className='main'>
-            <div className='m-4 w-[500px]'>
-                <h1 className='text-[62px] text-[#f9f9f9] font-bold '>Serving you since 1989.</h1>
-                <p className='text-[18px] text-[#f9f9f9] w-[55%]'>Acme Outdoors is an outdoor and adventure shop located in the Boathouse District in Oklahoma City.</p>
+            <div className='m-4 w-[100]'>
+                <h1 className='text-[62px] font-bold '>Simple UI Components </h1>
+                <p className='text-[18px] text-[#f9f9f9] w-[55%]'>This Project Contains the Collection of UI Components that I am creating using Next JS and Tailwind with various Libraries.</p>
                 <div className='mt-3'>
-                    <a className='bg-transparent border-[#f9f9f9] rounded border py-[5px] px-[25px] text-[#f9f9f9] border-solid  ' href='/'>Shop Merch</a>
+                    <a target="_blank" className='bg-transparent border-[#f9f9f9] rounded border py-[5px] px-[25px] text-[#f9f9f9] border-solid  ' href='https://github.com/SattyamSamania/UI-Components'>Github Link</a>
                 </div>
             </div>
         </div>
