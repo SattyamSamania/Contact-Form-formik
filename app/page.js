f07d129e7b8bbfcce98082cd3a587e3c3e5cfc42
SattyamSamania <Sattyamsamania2810@gmail.com>
2024-01-22 12:09:37 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function page() {
         <div className='main'>
             <div className='m-4 w-[100]'>
                 <h1 className='text-[62px] font-bold '>Simple UI Components </h1>
-                <p className='text-[18px] text-[#f9f9f9] w-[55%]'>This Project Contains the Collection of UI Components that I am creating using Next JS and Tailwind with various Libraries.</p>
+                <p className='text-[18px] text-[#f9f9f9] w-[50]'>This Project Contains the Collection of UI Components that I am creating using Next JS and Tailwind with various Libraries.</p>
                 <div className='mt-3'>
                     <a target="_blank" className='bg-transparent border-[#f9f9f9] rounded border py-[5px] px-[25px] text-[#f9f9f9] border-solid  ' href='https://github.com/SattyamSamania/UI-Components'>Github Link</a>
                 </div>
