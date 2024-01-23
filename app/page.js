8db443358a9313ccf18f10bd82547c54ebc25301
SattyamSamania <Sattyamsamania2810@gmail.com>
2024-01-23 16:39:15 +0530

    Updated Home Page

--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@
 export default function Page() {
     return (
         <div className='main '>
-            <div className='m-4 w-[700px] rounded max-w-screen-lg bg-[rgba(255,255,255,0.5)]  p-2 text-black'>
-                <h1 className='text-5xl font-bold mb-4 '>Simple UI Components </h1>
+            <div className='m-4 w-[500px] sm:w-[700px] rounded max-w-screen-lg bg-[rgba(255,255,255,0.5)]  p-2 text-black'>
+                <h1 className='sm:text-5xl text-3xl  font-bold mb-4 '>Simple UI Components </h1>
                 <p className='text-xl mb-4 '>This Project Contains the Collection of UI Components that I am creating using Next JS and Tailwind with various Libraries.</p>
                 <div className='mb-4 text-black'>
                     <a target="_blank" className='bg-transparent border-black rounded border py-2 px-4 text-black border-solid' href='https://github.com/SattyamSamania/UI-Components'>Github Link</a>
